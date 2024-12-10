411407983997eb64d8d8f91e5d7ea1c2695584bc
LAITHABBADI <91504332+LAITHABBADI@users.noreply.github.com>
2024-12-10 19:00:04 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import Footer from "@/components/layout/Footer";
 const font = Mulish({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-    title: "AI Agents as a Service - No Code AI AGENT BUILDER",
+    title: "AI Agents as a Service - No Code AI AGENT BUILDER - Custom GPT CHATBOT",
     description: "NexAgents your Aaas and AI Agent builder",
 };
 
