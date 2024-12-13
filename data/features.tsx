2ffd2ab658f5e44885804905e66875047c693d99
LAITHABBADI <91504332+LAITHABBADI@users.noreply.github.com>
2024-12-13 23:51:33 +0300

    Update features.tsx

--- data/features.tsx
+++ data/features.tsx
@@ -3,18 +3,18 @@ import {Handshake} from "lucide-react";
 
 export const features = [
     {
-        title: "Agent as a service",
-        description: "We provide fast and reliable service to our customers. Our team is always ready to help you with any problem you may have.",
+        title: "Free custom GPT CHATBOT",
+        description: "Build your own custom gpt 4 chatbot",
         icon: <RocketIcon className="w-6 h-6"/>,
     },
     {
         title: "Agent as a service",
-        description: "Our service is highly customizable. You can choose the color scheme, and other options that best suit your needs.",
+        description: "Our Aaas, Agent as a service is one for a kind, implement and distribute  vertical agents",
         icon: <Pencil2Icon className="w-6 h-6"/>,
     },
-    {
-        title: "Agent as a service",
-        description: "Our service is designed to be user friendly. You can easily navigate through the app and find what you need.",
+    
+        title: "AI AGENTS WITH ACTIONS",
+        description: "AI agents are autonomous programs that can observe their environment, make decisions, and take actions to achieve specific goals.",
         icon: <Handshake className="w-6 h-6"/>,
     },
 ];
